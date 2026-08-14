2616acaa2cd09d7dab3b41c57a3b6f568e530bf7
Alex <alex.hawley@vercel.com>
2026-08-14 18:10:45 -0500

    updated remote patterns for sfcc domain

--- next.config.ts
+++ next.config.ts
@@ -9,15 +9,14 @@ export default {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "zylq-002.dx.commercecloud.salesforce.com",
+        protocol: 'https',
+        hostname: process.env.SFCC_SANDBOX_DOMAIN
       },
       {
         protocol: "https",
         hostname: "edge.disstg.commercecloud.salesforce.com",
       },
       {
-        // Salesforce public demo backend (PWA Kit demo instance)
         protocol: "https",
         hostname: "zzrf-001.dx.commercecloud.salesforce.com",
       },
